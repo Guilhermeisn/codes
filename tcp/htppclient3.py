d9757bb84565925f20ad73ae8f9eacb84516aaaa
Guilhermeisn <guilherme331b@gmail.com>
2026-06-13 18:27:40 -0300

    Atividade chunk

--- tcp/htppclient3.py
+++ tcp/htppclient3.py
@@ -1,6 +1,14 @@
 import sys
 import socket
 
+def recv_line(sock):
+    line = b""
+
+    while not line.endswitch(b"\r\n"):
+        line += sock.recv(1)
+
+    return line
+
 def get_data(site, resource, output):
     my_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     my_sock.connect((site, 80))
@@ -31,8 +39,56 @@ def get_data(site, resource, output):
         print (f"Arquivo salvo em {output}")
     else:
         print ("Content-Length não encontrado!")
-        print ("Transferência baseado em chunks não implementado (ainda)!")
-        my_sock.close()
+        
+        transfer_chunked = False
+
+        for header in headers[1:]:
+            header = header.lower()
+
+            if header.startswith(b"transfer-encoding"):
+                if b"chunked" in header:
+                    transfer_chunked = True
+                    
+        if transfer_chunked:
+            print("Transfer-encoding: chunk detectado.")
+
+            body = data[pos2NL+4:]
+
+            arquivo = b""
+
+            while True:
+
+                while b"\r\n" not in body:
+                    body += my_sock.recv(4096)
+
+                pos = body.find(b"\r\n")
+
+                tamanho_hex = body[:pos]
+                body = body[pos+2:]
+
+                tamanho_chunk = int(tamanho_hex, 16)
+
+                print(f"Chunk recebeido: {tamanho_chunk} bytes. ")
+
+                if tamanho_chunk == 0:
+                    break
+
+                while len(body) < tamanho_chunk + 2:
+                    body += my_sock.recv(4096)
+
+                arquivo += body[:tamanho_chunk]
+
+                body = body[tamanho_chunk+2:]
+
+            fd = open(output, "wb")
+            fd.write(arquivo)
+            fd.close()
+
+            print(f"Arquivo salvo em {output}")
+        else:
+            print(f"[ERRO]: Tipo de transferência não suportado. ")
+
+    my_sock.close()
 
 if len(sys.argv) == 4:
     get_data(sys.argv[1], sys.argv[2], sys.argv[3])
